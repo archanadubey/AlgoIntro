93f37a57f62b78fe69fc8238c78f735aeb64b329
Archana <archooworship@gmail.com>
2026-02-20 22:56:50 -0800

    home work pushed

---
+++ src/ControlStatements.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class ControlStatements {
+
+    // take number from user and print its negative or positive
+    public static void main(String[] args) {
+
+//        Scanner sc = new Scanner(System.in);
+//
+//        System.out.println("Enter the Number to Check:  ");
+//
+//        int number = sc.nextInt();
+//
+//        if( number >= 1 ){
+//            System.out.println("Number is positive: "+ number);
+//        }else
+//            System.out.println("Number is negative: "+ number);
+//
+        Scanner sca = new Scanner(System.in);
+        System.out.println("Enter the Number to Check:  ");
+        int weekNumber = sca.nextInt();
+        weekDaysNameSwitch(weekNumber);
+
+    }
+    public static void weekDaysNameSwitch(int number) {
+
+        if (number < 1 | number > 7 )
+            System.out.println("Please Enter Valid Week Number to get the WeekDays: ");
+
+        switch(number){
+            case 1:
+                System.out.println("Sunday");
+                break;
+            case 2:
+                System.out.println("Monday");
+                break;
+            case 3:
+                System.out.println("Tuesday");
+                break;
+            case 4:
+                System.out.println("Wednesday");
+                break;
+            case 5:
+                System.out.println("Thursday");
+                break;
+            case 6:
+                System.out.println("Friday");
+                break;
+            case 7:
+                System.out.println("Saturday");
+                break;
+
+        }
+
+    }
+}
